84c1cc53cdb4c72c0fa28c3d7266fd206a5edad2
Toru Furukawa <torufurukawa@gmail.com>
2021-07-21 08:58:34 +0900

    Add tests

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 const test = require('ava')
+const sinon = require('sinon')
+let stripe = require('./lib/stripe.js')
 
 test('foo', t => {
   t.pass()
@@ -7,4 +9,10 @@ test('foo', t => {
 test('bar', async t => {
   const bar = Promise.resolve('bar')
   t.is(await bar, 'bar')
+})
+
+test('fake', t => {
+  const fake = sinon.replace(stripe.accounts, 'create', sinon.fake.returns('fake'))
+  t.is(fake(), 'fake')
+  t.is(fake.callCount, 1)
 })
